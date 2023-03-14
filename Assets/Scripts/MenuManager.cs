66d8342c717f1a29d2f041cd863877aa9f4469c8
Subhajit-Ghosh14 <subhajit14capri@gmail.com>
2023-03-14 16:19:49 +0530

    1st commit
    1st change

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager Instance;
+    public TextMeshProUGUI nameText;
+    public string text;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        text= nameText.text;
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
